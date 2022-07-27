5906380aecda991e38cb5586a1af18418676e0d3
Tibor Répási <rtib@users.noreply.github.com>
2022-07-27 23:39:44 +0200

    refactoring display control

---
+++ Geiger-Mueller/src/display.cpp
@@ -0,0 +1,51 @@
+#include <U8g2lib.h>
+
+class Display {
+  public:
+    Display(const char* version) {
+      u8g2 = U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C(U8G2_R0, /* clock=*/ 5, /* data=*/ 4, /* reset=*/ 16);
+      u8g2.begin();
+      versionString = version;
+    }
+
+    void splash() {
+      u8g2.clearBuffer();
+      drawSplash();
+      u8g2.sendBuffer();
+    }
+
+    void initScreen(int current, int from) {
+      u8g2.clearBuffer();
+      drawSplash();
+      u8g2.drawFrame(8,25,114,5);
+      u8g2.drawHLine(10, 27, 110 * (from - current + 1) / from);
+      u8g2.sendBuffer();
+    }
+
+    void runDisplay(double exposure, double dose, unsigned long cpm) {
+      u8g2.clearBuffer();
+      u8g2.setFont(u8g2_font_ncenR18_tf);
+      u8g2.setCursor(0, 20);
+      u8g2.print(exposure);
+      u8g2.print(F(" \xB5Sv/h"));
+
+      u8g2.setFont(u8g2_font_5x8_tf);
+      u8g2.setCursor(0, 31);
+      u8g2.print(dose); u8g2.print(F(" \xB5Sv"));
+
+      u8g2.setCursor(75, 31);
+      u8g2.print(F("CPM: ")); u8g2.print(cpm);
+      u8g2.sendBuffer();  
+    }
+  private:
+    U8G2 u8g2;
+    const char* versionString;
+
+    void drawSplash() {
+      u8g2.setFont(u8g2_font_smart_patrol_nbp_tf);
+      u8g2.drawStr(10, 10, "Geiger-M\xFCller");
+      u8g2.setFont(u8g2_font_profont11_tr);
+      u8g2.drawStr(25, 21, "Version");
+      u8g2.drawStr(80, 21, versionString);
+    }
+};
--- Geiger-Mueller/src/main.cpp
+++ Geiger-Mueller/src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <ESP8266TimerInterrupt.h>
-#include <U8g2lib.h>
+
 #include <gmCounter.h>
+#include "display.cpp"
 
 #define VERSION "1.0"
 #define GM_PULSE_GPIO 2
 
-U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 ESP8266Timer timer;
 gmCounter counter(GM_TUBE_J305_CI);
+Display display(VERSION);
 
 IRAM_ATTR void gmPulse() {
   counter.tubePulse();
@@ -22,7 +23,7 @@ IRAM_ATTR void timerHandler() {
 void setup() {
 
   Serial.begin(9600);
-  u8g2.begin();
+  display.splash();
   delay(500);
 
   Serial.println(F("\n"));
@@ -36,48 +37,19 @@ void setup() {
   } else {
     Serial.println(F("Failed to start bucket timer."));
   }
-
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_smart_patrol_nbp_tf);
-  u8g2.drawStr(10, 10, "Geiger-M\xFCller");
-  u8g2.setFont(u8g2_font_profont11_tr);
-  u8g2.drawStr(25, 21, "Version");
-  u8g2.drawStr(80, 21, VERSION);
-  u8g2.sendBuffer();
 }
 
 void loop() {
   delay(1000);
-  Serial.printf("count: %ld\tdose: %lf uSv", counter.getCounts(), counter.calcDose());
+  Serial.printf("%ld: counts: %ld\tdose: %lf uSv", millis(), counter.getCounts(), counter.calcDose());
   if (counter.getRemainingInit()) {
     Serial.print(F("\t.INIT."));
 
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_smart_patrol_nbp_tf);
-    u8g2.drawStr(10,10,"Geiger-M\xFCller");
-    u8g2.setFont(u8g2_font_profont11_tr);
-    u8g2.drawStr(25, 21, "Version");
-    u8g2.drawStr(80, 21, VERSION);
-    u8g2.drawFrame(8,25,114,5);
-    u8g2.drawHLine(10, 27, 110 * (COUNTER_BUCKETS - counter.getRemainingInit() + 1) / COUNTER_BUCKETS);
-    u8g2.sendBuffer();
+    display.initScreen(counter.getRemainingInit(), COUNTER_BUCKETS);
   }
   else {
     Serial.printf("\tcpm: %ld\texposure: %lf uSv/h", counter.getCPM(), counter.calcExposure());
-
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_ncenR18_tf);
-    u8g2.setCursor(0, 20);
-    u8g2.print(counter.calcExposure());
-    u8g2.print(F(" \xB5Sv/h"));
-
-    u8g2.setFont(u8g2_font_5x8_tf);
-    u8g2.setCursor(0, 31);
-    u8g2.print(counter.calcDose()); u8g2.print(F(" \xB5Sv"));
-
-    u8g2.setCursor(75, 31);
-    u8g2.print(F("CPM: ")); u8g2.print(counter.getCPM());
-    u8g2.sendBuffer();  
+    display.runDisplay(counter.calcExposure(), counter.calcDose(), counter.getCPM());
   }
   Serial.println();
 }
